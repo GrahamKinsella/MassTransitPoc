f80d4fed067cd8af6026dcd98a5db7e0eee79384
Graham Kinsella <gkinsella@eshopworld.com>
2023-08-06 20:33:10 +0100

    remove unused events

--- MassTransitPoc/Domain/DomainEvents.cs
+++ MassTransitPoc/Domain/DomainEvents.cs
@@ -17,23 +17,6 @@ public record InviteUpdatedEvent
     public string Status { get; set; }
 }
 
-public record BrandCreatedEvent
-{
-    //Operationid from 
-    public Guid OperationId { get; set; }
-    public string TenantCode {get; set; }
-}
-
-public record UserCreatedEvent
-{
-    public Guid OperationId { get; set; }
-}
-
-public record EmailSentEvent
-{
-    public Guid OperationId { get; set; }
-}
-
 public record InviteFailedEvent
 {
     public Guid OperationId { get; set; }
